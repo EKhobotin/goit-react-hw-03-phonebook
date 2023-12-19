63fb3af57f2d878b13b6646b702c0718b1dc1e0f
EKhobotin <evgen.29.3.85@gmail.com>
2023-12-19 13:04:50 +0200

    add LS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,17 +3,36 @@ import { nanoid } from 'nanoid';
 import { FormAddContact } from './FormAddContact/FormAddContact';
 import { ContactsList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
+import { loadFromLs, saveToLs } from 'helper/localStorage';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: loadFromLs('contacts') ? loadFromLs('contacts') : [],
+    //   [
+    //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    // ],
     filter: '',
   };
+
+  componentDidMount() {
+    // const savedContacts = loadFromLs('contacts');
+    // if (savedContacts)
+    //   this.setState({ contacts: savedContacts }, () => {
+    //     console.log(this.state.contacts);
+    //   });
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts.length !== prevState.contacts.length) {
+      saveToLs(this.state.contacts);
+    }
+    // console.log('up');
+    // console.log(prevState);
+    // console.log(this.state);
+  }
 
   handleAddContact = (name, number) => {
     if (this.state.contacts.find(el => el.name === name)) {
